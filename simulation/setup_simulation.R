f8c55cdb7b73ff49622898d2a192cc7b58513c6e
kylestanley1313 <kylestanley1313@gmail.com>
2023-01-05 15:34:00 -0500

    creates config_map.csv in setup script

--- simulation/setup_simulation.R
+++ simulation/setup_simulation.R
@@ -18,6 +18,13 @@ dir.create(file.path('simulation', 'data', args$design.id))
 dir.create(file.path('simulation', 'results', args$design.id))
 dir.create(file.path(design$scratch_root, 'simulation', 'data', args$design.id))
 
+## Iniitalize config map
+col.names <- c(
+  'config.id', 'l.scheme', 'e.scheme', 'K', 'delta', 'n', 
+  'l.scale.min', 'l.scale.max', 'e.scale.min', 'e.scale.max', 'M', 'J'
+  )
+config.map <- data.frame(matrix(nrow = 0, ncol = length(col.names)))
+colnames(config.map) <- col.names
 
 config.num <- 1
 for (l.scheme in design$loading_schemes) {
@@ -27,14 +34,14 @@ for (l.scheme in design$loading_schemes) {
         for (J in design$Js) {
           for (delta in design$deltas) {
             for (e.scale.range in design$error_scale_ranges) {
-              for (N in design$num_samps) {
+              for (n in design$num_samps) {
                 
                 config.id <- str_glue('config-{config.num}')
                 
                 dir.data <- file.path('simulation', 'data', args$design.id, config.id)
                 dir.scratch.data <- file.path(design$scratch_root, dir.data)
                 dir.results <- file.path('simulation', 'results', args$design.id, config.id)
-                dir.create(dir.data)
+                # dir.create(dir.data)
                 if (!dir.exists(dir.scratch.data)) dir.create(dir.scratch.data)
                 dir.create(dir.results)
                 
@@ -55,7 +62,7 @@ for (l.scheme in design$loading_schemes) {
                     delta = delta,
                     delta_est = design$delta_est,
                     error_scale_range = e.scale.range,
-                    num_samps = N
+                    num_samps = n
                   ),
                   tuning = list(
                     train_prop = design$train_prop,
@@ -67,7 +74,15 @@ for (l.scheme in design$loading_schemes) {
                     )
                   )
                 )
-                write_yaml(config, file.path(dir.data, 'config.yml'))
+                # write_yaml(config, file.path(dir.data, 'config.yml'))
+                
+                row <- c(
+                  config.num, l.scheme, e.scheme, K, delta, n, 
+                  l.scale.range[1], l.scale.range[2], 
+                  e.scale.range[1], e.scale.range[2], 
+                  design$M, J
+                )
+                config.map[nrow(config.map)+1,] <- row
                 
                 config.num <- config.num + 1
                 
@@ -79,5 +94,15 @@ for (l.scheme in design$loading_schemes) {
     }
   }
 }
+
+write.table(
+  config.map, 
+  file.path('simulation', 'results', args$design.id, 'config_map.csv'), 
+  sep = ',',
+  col.names = TRUE,
+  row.names = FALSE,
+  append = FALSE,
+  quote = FALSE
+)
 
 
