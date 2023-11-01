e9a37db82f3ef85eeedf250c056f81d783f58409
kylestanley1313 <kylestanley1313@gmail.com>
2023-11-01 10:49:27 -0400

    fixes file names

--- slurm-scripts/analysis.sh
+++ slurm-scripts/analysis.sh
@@ -6,7 +6,7 @@
 #SBATCH -N 1                                      
 #SBATCH -n 1                                  
 #SBATCH --mem-per-cpu=5gb                         
-#SBATCH --time=36:00:00                           
+#SBATCH --time=00:10:00                           
 #SBATCH --output=analysis-main_%j.log
 
 # Set variables
@@ -20,10 +20,10 @@ cd $WORK_ROOT/slurm-scripts
 
 JOB_ID=$(sbatch --parsable analysis-1-preprocessing.sh $ACCOUNT $EMAIL $WORK_ROOT $SCRATCH_ROOT $ANALYSIS_ID)
 JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-2-splitting.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
-JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-collab-3-alpha-testing.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
-JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-collab-4-scree-plot.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
-JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-collab-5-estimation.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
-JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-collab-6-postprocessing.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
-JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-collab-7-ica.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
-sbatch --dependency=afterok:$JOB_ID analysis-collab-7-ica.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID
+JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-3-alpha-testing.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
+JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-4-scree-plot.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
+JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-5-estimation.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
+JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-6-postprocessing.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
+JOB_ID=$(sbatch --parsable --dependency=afterok:$JOB_ID analysis-7-ica.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID)
+sbatch --dependency=afterok:$JOB_ID analysis-7-ica.sh $ACCOUNT $EMAIL $WORK_ROOT $ANALYSIS_ID
 
